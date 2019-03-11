14c81b8d6aeb4effeb9f9b2594c124acc768e807
Fred Duby <fcd1@columbia.edu>
2019-03-11 18:33:04 -0400

    Add sidebar to series file-and-box-number view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   get 'asi/as_ead_from_fixture'
   get 'asi/as_ead'
 
-  get '/asi/repo_id/:repo_id/res_id/:res_id/ead' => 'asi#as_ead'
-  get '/asi/local_fixtures/repo_id/:repo_id/res_id/:res_id/ead_fixture' => 'asi#as_ead_from_local_fixture'
+  get '/asi/repo_id/:repo_id/res_id/:res_id/ead',
+      to: 'asi#as_ead',
+      as: 'ead'
+  get '/asi/local_fixtures/repo_id/:repo_id/res_id/:res_id/ead_fixture',
+      to: 'asi#as_ead_from_local_fixture',
+      as: 'ead_fixture'
 
   get '/asi/repo_id/:repo_id/res_id/:res_id/ead/ser_id/:ser_id/series' => 'asi#as_ead_series'
   get '/asi/local_fixtures/repo_id/:repo_id/res_id/:res_id/ead_fixture/ser_id/:ser_id/series',
