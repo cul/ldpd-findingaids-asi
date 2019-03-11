6e54b8c30b499202a1df82f20f41fd778b2ba414
Fred Duby <fcd1@columbia.edu>
2019-03-11 19:05:20 -0400

    Pass series slug to summary partial

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
       to: 'asi#as_ead_from_local_fixture',
       as: 'ead_fixture'
 
-  get '/asi/repo_id/:repo_id/res_id/:res_id/ead/ser_id/:ser_id/series' => 'asi#as_ead_series'
+  get '/asi/repo_id/:repo_id/res_id/:res_id/ead/ser_id/:ser_id/series',
+      to: 'asi#as_ead_series',
+      as: 'series'
   get '/asi/local_fixtures/repo_id/:repo_id/res_id/:res_id/ead_fixture/ser_id/:ser_id/series',
       to: 'asi#as_ead_series_from_local_fixture',
       as: 'series_fixture'
