d4a3eb9911ccd34196c95797c5ee246e3fa6214e
Fred Duby <fcd1@columbia.edu>
2019-03-18 18:07:03 -0400

    Factor out series processing, WIP

---
+++ lib/asi/as_ead_series.rb
@@ -0,0 +1,47 @@
+require 'net/http'
+
+module Asi
+  class AsEadSeries
+    XPATH = {
+      title: './xmlns:did/xmlns:unittitle',
+      scope_content: './xmlns:scopecontent/xmlns:p'
+    }
+
+    attr_reader *XPATH.keys
+
+    XPATH.keys.each do |attr|
+      define_method :"debug_#{attr}" do
+        "#{self.send(attr)} DEBUG: #{XPATH[attr]}"
+      end
+    end
+
+    # Takes a Nokogiri::XML::Element (fcd1: verify this)
+    # containing a <c lelvel="series"> element
+    def parse(nokogiri_xml)
+      @title = nokogiri_xml.xpath(XPATH[:title]).text
+      @scope_content = nokogiri_xml.xpath(XPATH[:scope_content]).text
+    end
+
+    def generate_html_from_component(component_arg, html_out = '')
+      # files = component.xpath('./xmlns:c[@level="file"]')
+      components = component_arg.xpath('./xmlns:c')
+      return if components.empty?
+      html_out << '<div class="component_entry" style="margin-left:2em;">'
+      # puts series_files.inspect
+      components.each do |component|
+        title = component.xpath('./xmlns:did/xmlns:unittitle').text
+        container_number = component.xpath('./xmlns:did/xmlns:container').text
+        scope_content = component.xpath('./xmlns:scopecontent/xmlns:p').text
+        html_out << '<p style="margin:0">'
+        html_out << '<span style="text-align:left;">' << title << '</span>'
+        html_out << '<span style="text-align:right;float:right;">' << container_number << '</span>'
+        html_out << '</p>'
+        html_out << '<p style="margin:0">' << scope_content << '</p>'
+        # file_subcomponents = file.xpath('./xmlns:c[@level="file"]')
+        # generate_html_from_component(file_subcomponents) unless file_subcomponents.empty?
+        generate_html_from_component(component, html_out)
+      end
+      html_out << '</div>'
+    end
+  end
+end
---
+++ spec/asi/as_ead_series_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+require 'asi/as_ead.rb'
+require 'asi/as_ead_series.rb'
+
+RSpec.describe Asi::AsEadSeries do
+  ########################################## API/interface
+  describe 'API/interface' do
+    context 'has attr_reader for instance var' do
+      # within a given series: <did>:<unittitle>
+      it 'title' do
+        expect(subject).to respond_to(:title)
+      end
+
+      # within a given series: <scopecontent>:<p>
+      it 'scope_content' do
+        expect(subject).to respond_to(:scope_content)
+      end
+    end
+
+    context 'has parse method' do
+      it 'that takes one argument' do
+        expect(subject).to respond_to(:parse).with(1).argument
+      end
+    end
+
+    context 'has generate_html_from_component method' do
+      it 'that takes two arguments' do
+        expect(subject).to respond_to(:generate_html_from_component).with(2).arguments
+      end
+    end
+  end
+
+  ########################################## Debug API/interface
+  describe 'debug API/interface' do
+    context 'has debug_attr_reader for instance var' do
+      # within a given series: <did>:<unittitle>
+      it 'title' do
+        expect(subject).to respond_to(:debug_title)
+      end
+
+      # within a given series: <scopecontent>:<p>
+      it 'scope_content' do
+        expect(subject).to respond_to(:debug_scope_content)
+      end
+    end
+  end
+
+  ########################################## Functionality
+  describe 'Testing functionality: ' do
+    ########################################## generate_html_from_components
+    context 'generate_html_from_components' do
+      before(:example) do
+        @as_ead = Asi::AsEad.new
+        xml_input = fixture_file_upload('asi/as_ead_resource_4767_representation.xml').read
+        @nokogiri_xml = Nokogiri::XML(xml_input)
+        @as_ead.parse_arch_desc_dsc @nokogiri_xml
+      end
+
+      # TODO: Fix/Change/Finish following. Probably requires changes in above before block
+      it 'process files (CHANGE)' do
+        tested_series = @as_ead.archive_dsc_series[0]
+        # puts '*******************************************'
+        #  puts '*******************************************'
+        # puts @as_ead.generate_html_from_component(tested_series, '')
+        # puts @as_ead.process_children_files(series_c_children)
+        # puts tested
+        # expect(tested).to include({:title => 'Price, Arthur: to Rockwell Kent, t.l.s., 15', :box_number => '3'})
+        expect(true).to eq true
+      end
+    end
+
+    ########################################## parse
+    context 'parse' do
+      before(:example) do
+        @as_ead = Asi::AsEad.new
+        xml_input = fixture_file_upload('asi/as_ead_resource_4767_representation.xml').read
+        @nokogiri_xml = Nokogiri::XML(xml_input)
+        @as_ead.parse_arch_desc_dsc @nokogiri_xml
+        @as_ead_series = Asi::AsEadSeries.new
+        @as_ead_series.parse @as_ead.archive_dsc_series[0]
+      end
+
+      it 'sets the title correctly' do
+        tested = @as_ead_series.title
+        expect(tested).to eq 'Series I: Cataloged Correspondence'
+      end
+
+      it 'sets the scope content correctly' do
+        tested = @as_ead_series.scope_content
+        expect(tested).to include "Kent's letters are arranged chronologically in Boxes 2: (1918-1940); 3: (1941-1969)"
+      end
+    end
+  end
+end
