07aa2f25aacf4b952359e1010aa8573dbff81f17
Fred Duby <fcd1@columbia.edu>
2019-03-25 16:16:23 -0400

    Cleanup vars in controller

--- app/controllers/asi_controller.rb
+++ app/controllers/asi_controller.rb
@@ -4,118 +4,118 @@ require 'asi/as_ead_component'
 
 class AsiController < ApplicationController
   def as_ead
-    @asi_api = Asi::AsApi.new
-    @input_xml = @asi_api.get_ead_resource_description(params[:repo_id],params[:res_id])
+    @as_api = Asi::AsApi.new
+    @input_xml = @as_api.get_ead_resource_description(params[:repo_id],params[:res_id])
     ead_set_properties
   end
 
   def as_ead_debug
-    @asi_api = Asi::AsApi.new
-    @input_xml = @asi_api.get_ead_resource_description(params[:repo_id],params[:res_id])
+    @as_api = Asi::AsApi.new
+    @input_xml = @as_api.get_ead_resource_description(params[:repo_id],params[:res_id])
     ead_set_properties_debug
   end
 
   def as_ead_from_local_fixture
-    @asi_api = Asi::AsApi.new
+    @as_api = Asi::AsApi.new
     @input_xml =
-      @asi_api.get_ead_resource_description_from_local_fixture(params[:repo_id],params[:res_id])
+      @as_api.get_ead_resource_description_from_local_fixture(params[:repo_id],params[:res_id])
     ead_set_properties
   end
 
   def as_ead_from_local_fixture_debug
-    @asi_api = Asi::AsApi.new
+    @as_api = Asi::AsApi.new
     @input_xml =
-      @asi_api.get_ead_resource_description_from_local_fixture(params[:repo_id],params[:res_id])
+      @as_api.get_ead_resource_description_from_local_fixture(params[:repo_id],params[:res_id])
     ead_set_properties_debug
   end
 
   def as_ead_series
-    @asi_api = Asi::AsApi.new
-    @input_xml = @asi_api.get_ead_resource_description(params[:repo_id],params[:res_id])
+    @as_api = Asi::AsApi.new
+    @input_xml = @as_api.get_ead_resource_description(params[:repo_id],params[:res_id])
     ead_series_set_properties params[:ser_id]
   end
 
   def as_ead_series_from_local_fixture
-    @asi_api = Asi::AsApi.new
+    @as_api = Asi::AsApi.new
     @input_xml =
-      @asi_api.get_ead_resource_description_from_local_fixture(params[:repo_id],params[:res_id])
+      @as_api.get_ead_resource_description_from_local_fixture(params[:repo_id],params[:res_id])
     ead_series_set_properties params[:ser_id]
   end
 
   def as_ead_from_fixture
-    @asi_api = Asi::AsApi.new
-    @input_xml = @asi_api.get_ead_resource_description_from_fixture
+    @as_api = Asi::AsApi.new
+    @input_xml = @as_api.get_ead_resource_description_from_fixture
     ead_set_properties
   end
 
   private
   def ead_set_properties
-    @asi_ead = Asi::AsEad.new @input_xml
-    @ead_title = @asi_ead.archive_title
-    @ead_abstract = @asi_ead.archive_abstract
-    @ead_bib_id = @asi_ead.archive_id
-    @ead_creators = @asi_ead.get_creators
-    @ead_unit_date = @asi_ead.archive_date
-    @ead_physical_description = @asi_ead.archive_physical_description_extent_carrier
-    @ead_language = @asi_ead.archive_language
-    @ead_access_restrictions_head = @asi_ead.archive_access_restrictions_head
-    @ead_access_restrictions_value = @asi_ead.archive_access_restrictions_value
-    @ead_scope_content_head = @asi_ead.archive_scope_content_head
-    @ead_scope_content_value = @asi_ead.archive_scope_content_value
-    @ead_repository_corpname = @asi_ead.archive_repository
-    @ead_preferred_citation_head = @asi_ead.archive_preferred_citation_head
-    @ead_preferred_citation_value = @asi_ead.archive_preferred_citation_value
-    @ead_use_restrictions_head = @asi_ead.archive_use_restrictions_head
-    @ead_use_restrictions_value = @asi_ead.archive_use_restrictions_value
-    @ead_processing_information_head = @asi_ead.archive_processing_information_head
-    @ead_processing_information_value = @asi_ead.archive_processing_information_value
-    @ead_biography_history_head = @asi_ead.archive_biography_history_head
-    @ead_biography_history_value = @asi_ead.archive_biography_history_value
-    @ead_series_titles = @asi_ead.archive_dsc_series_titles
-    @ead_series_scope_content = @asi_ead.get_series_scope_content
-    @ead_subjects = @asi_ead.get_subjects
-    @ead_genres_forms = @asi_ead.get_genres_forms
+    @ead = Asi::AsEad.new @input_xml
+    @title = @ead.archive_title
+    @abstract = @ead.archive_abstract
+    @bib_id = @ead.archive_id
+    @creators = @ead.get_creators
+    @unit_date = @ead.archive_date
+    @physical_description = @ead.archive_physical_description_extent_carrier
+    @language = @ead.archive_language
+    @access_restrictions_head = @ead.archive_access_restrictions_head
+    @access_restrictions_value = @ead.archive_access_restrictions_value
+    @scope_content_head = @ead.archive_scope_content_head
+    @scope_content_value = @ead.archive_scope_content_value
+    @repository_corpname = @ead.archive_repository
+    @preferred_citation_head = @ead.archive_preferred_citation_head
+    @preferred_citation_value = @ead.archive_preferred_citation_value
+    @use_restrictions_head = @ead.archive_use_restrictions_head
+    @use_restrictions_value = @ead.archive_use_restrictions_value
+    @processing_information_head = @ead.archive_processing_information_head
+    @processing_information_value = @ead.archive_processing_information_value
+    @biography_history_head = @ead.archive_biography_history_head
+    @biography_history_value = @ead.archive_biography_history_value
+    @series_titles = @ead.archive_dsc_series_titles
+    @series_scope_content = @ead.get_series_scope_content
+    @subjects = @ead.get_subjects
+    @genres_forms = @ead.get_genres_forms
   end
 
   def ead_set_properties_debug
-    @asi_ead = Asi::AsEad.new @input_xml
-    @ead_title = @asi_ead.debug_archive_title
-    @ead_abstract = @asi_ead.debug_archive_abstract
-    @ead_bib_id = @asi_ead.debug_archive_id
-    @ead_creators = @asi_ead.get_creators
-    @ead_unit_date = @asi_ead.debug_archive_date
-    @ead_physical_description = @asi_ead.debug_archive_physical_description_extent_carrier
-    @ead_language = @asi_ead.debug_archive_language
-    @ead_access_restrictions_head = @asi_ead.debug_archive_access_restrictions_head
-    @ead_access_restrictions_value = @asi_ead.debug_archive_access_restrictions_value
-    @ead_scope_content_head = @asi_ead.debug_archive_scope_content_head
-    @ead_scope_content_value = @asi_ead.debug_archive_scope_content_value
-    @ead_repository_corpname = @asi_ead.debug_archive_repository
-    @ead_preferred_citation_head = @asi_ead.debug_archive_preferred_citation_head
-    @ead_preferred_citation_value = @asi_ead.debug_archive_preferred_citation_value
-    @ead_use_restrictions_head = @asi_ead.debug_archive_use_restrictions_head
-    @ead_use_restrictions_value = @asi_ead.debug_archive_use_restrictions_value
-    @ead_processing_information_head = @asi_ead.debug_archive_processing_information_head
-    @ead_processing_information_value = @asi_ead.debug_archive_processing_information_value
-    @ead_biography_history_head = @asi_ead.debug_archive_biography_history_head
-    @ead_biography_history_value = @asi_ead.debug_archive_biography_history_value
+    @ead = Asi::AsEad.new @input_xml
+    @title = @ead.debug_archive_title
+    @abstract = @ead.debug_archive_abstract
+    @bib_id = @ead.debug_archive_id
+    @creators = @ead.get_creators
+    @unit_date = @ead.debug_archive_date
+    @physical_description = @ead.debug_archive_physical_description_extent_carrier
+    @language = @ead.debug_archive_language
+    @access_restrictions_head = @ead.debug_archive_access_restrictions_head
+    @access_restrictions_value = @ead.debug_archive_access_restrictions_value
+    @scope_content_head = @ead.debug_archive_scope_content_head
+    @scope_content_value = @ead.debug_archive_scope_content_value
+    @repository_corpname = @ead.debug_archive_repository
+    @preferred_citation_head = @ead.debug_archive_preferred_citation_head
+    @preferred_citation_value = @ead.debug_archive_preferred_citation_value
+    @use_restrictions_head = @ead.debug_archive_use_restrictions_head
+    @use_restrictions_value = @ead.debug_archive_use_restrictions_value
+    @processing_information_head = @ead.debug_archive_processing_information_head
+    @processing_information_value = @ead.debug_archive_processing_information_value
+    @biography_history_head = @ead.debug_archive_biography_history_head
+    @biography_history_value = @ead.debug_archive_biography_history_value
     # fcd1, 03/15/19: TODO: see if debug info for titles is feasible.
-    @ead_series_titles = @asi_ead.archive_dsc_series_titles
-    @ead_series_scope_content = @asi_ead.get_series_scope_content
-    @ead_subjects = @asi_ead.get_subjects
-    @ead_genres_forms = @asi_ead.get_genres_forms
+    @series_titles = @ead.archive_dsc_series_titles
+    @series_scope_content = @ead.get_series_scope_content
+    @subjects = @ead.get_subjects
+    @genres_forms = @ead.get_genres_forms
   end
 
-  def ead_series_set_properties series_num
-    @asi_ead = Asi::AsEad.new @input_xml
-    @ead_series_files_info = @asi_ead.get_files_info_for_series series_num
+  def ead_series_set_properties component_num
+    @ead = Asi::AsEad.new @input_xml
+    @series_files_info = @ead.get_files_info_for_series component_num
     # @ead_series_titles is repeated in above method, so try to DRY
-    @ead_series_titles = @asi_ead.archive_dsc_series_titles
-    series_nokogiri_xml = @asi_ead.archive_dsc_series[series_num.to_i - 1]
-    @as_ead_series = Asi::AsEadComponent.new
-    @as_ead_series.parse series_nokogiri_xml
-    @series_title = @as_ead_series.title
-    @series_scope_content = @as_ead_series.scope_content
-    @series_html = @as_ead_series.generate_html
+    @series_titles = @ead.archive_dsc_series_titles
+    component_nokogiri_xml = @ead.archive_dsc_series[component_num.to_i - 1]
+    @component = Asi::AsEadComponent.new
+    @component.parse component_nokogiri_xml
+    @component_title = @component.title
+    @component_scope_content = @component.scope_content
+    @component_html = @component.generate_html
   end
 end
