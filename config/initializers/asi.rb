9aac9ebc62f130a53d62461d198f9adc2c8dd074
Fred Duby <fcd1@columbia.edu>
2019-04-15 13:54:35 -0400

    Rename AS config

--- config/initializers/asi.rb
+++ config/initializers/asi.rb
@@ -1,4 +1,4 @@
-ASI_CONFIG = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/archivespace.yml")[Rails.env]).freeze
+AS_CONFIG = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/archivespace.yml")[Rails.env]).freeze
 # REPOS contains info about the individual repositories, for example if aeon is used or not.
 # REPOS is indexed by the repository ID used by ArchiveSpace to indentify the individual repository.
 REPOS = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/repositories.yml")[Rails.env]).freeze
--- lib/archive_space/api/client.rb
+++ lib/archive_space/api/client.rb
@@ -6,7 +6,7 @@ module ArchiveSpace
       def get_ead_resource_description(repo_id, resource_id)
         # move following outside later
         authenticate
-        repo_url = "#{ASI_CONFIG[:repositories_url]}/#{repo_id}"
+        repo_url = "#{AS_CONFIG[:repositories_url]}/#{repo_id}"
         ead_resource_description_url = "#{repo_url}/resource_descriptions/#{resource_id}.xml"
         # puts ead_resource_description_url
         get_uri = URI(ead_resource_description_url)
@@ -34,9 +34,9 @@ module ArchiveSpace
       end
 
       def authenticate
-        post_uri = URI(ASI_CONFIG[:auth_url])
+        post_uri = URI(AS_CONFIG[:auth_url])
         post_request = Net::HTTP::Post.new post_uri.request_uri
-        post_request.set_form_data('password' => ASI_CONFIG[:password])
+        post_request.set_form_data('password' => AS_CONFIG[:password])
         result = Net::HTTP.start(post_uri.host, post_uri.port, use_ssl: true) do |http|
           http.request(post_request)
         end
@@ -49,8 +49,8 @@ module ArchiveSpace
       def get_resource(repo_id, resource_id)
         # move following outside later
         authenticate
-        repo_url = "#{ASI_CONFIG[:repositories_url]}/#{repo_id}"
-        params = ASI_CONFIG[:get_resource_params]
+        repo_url = "#{AS_CONFIG[:repositories_url]}/#{repo_id}"
+        params = AS_CONFIG[:get_resource_params]
         # puts params
         resource_url = "#{repo_url}/resources/#{resource_id}?#{params}"
         # puts resource_url
@@ -68,7 +68,7 @@ module ArchiveSpace
       def get_resource_tree(repo_id, resource_id)
         # move following outside later
         authenticate
-        repo_url = "#{ASI_CONFIG[:repositories_url]}/#{repo_id}"
+        repo_url = "#{AS_CONFIG[:repositories_url]}/#{repo_id}"
         resource_tree_url = "#{repo_url}/resources/#{resource_id}/tree"
         # puts resource_url
         get_uri = URI(resource_tree_url)
@@ -85,7 +85,7 @@ module ArchiveSpace
       def get_resource_tree_root(repo_id, resource_id)
         # move following outside later
         authenticate
-        repo_url = "#{ASI_CONFIG[:repositories_url]}/#{repo_id}"
+        repo_url = "#{AS_CONFIG[:repositories_url]}/#{repo_id}"
         resource_tree_url = "#{repo_url}/resources/#{resource_id}/tree/root"
         # puts resource_url
         get_uri = URI(resource_tree_url)
