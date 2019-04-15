36e43e4d640e773c424604ae0a6b0c8b836029fa
Fred Duby <fcd1@columbia.edu>
2019-04-15 12:41:45 -0400

    Validate repo code present in url

--- app/controllers/finding_aids_controller.rb
+++ app/controllers/finding_aids_controller.rb
@@ -1,7 +1,20 @@
 class FindingAidsController < ApplicationController
+  before_action :validate_repository_code_in_url
+
   def index
   end
 
   def show
+  end
+
+  private
+  def validate_repository_code_in_url
+    unless REPOS.key? params[:repository_id]
+      # not currently displaying contents of flash, but may be useful
+      # when redirect to other than root
+      flash[:error] = 'Non-existent repo code in url'
+      # for now, redirect to root. Change later
+      redirect_to '/'
+    end
   end
 end
