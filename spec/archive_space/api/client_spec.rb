e14a232f43a77a76c0bd77f91f4ae89f5ee11b4d
Fred Duby <fcd1@columbia.edu>
2019-04-22 09:36:37 -0400

    Fix typo in filename for API specs and update specs

--- spec/archive_space/api/client_spec.rb
+++ spec/archive_space/api/client_spec.rb
@@ -3,13 +3,17 @@ require 'archive_space/api/client.rb'
 
 RSpec.describe ArchiveSpace::Api::Client do
   context "API/interface" do
-    # http://archivesspace.github.io/archivesspace/api/#authentication
-    it 'has #get_ead_resource_description_from_fixture  method' do
-      # puts subject.use_local_as_ead_file.xpath('/xmlns:ead/xmlns:archdesc/xmlns:did/xmlns:unittitle').text
-      # puts subject.use_local_as_ead_file.css('ead archdesc did unittitle').first.text
-      expect(subject).to respond_to(:get_ead_resource_description_from_fixture).with(0).arguments
+    # http://archivesspace.github.io/archivesspace/api/#get-an-ead-representation-of-a-resource204
+    it 'has #get_ead_resource_description method' do
+      expect(subject).to respond_to(:get_ead_resource_description).with(2).arguments
     end
 
+    # Following uses local fixture file instead of API call. Used during dev and test
+    it 'has #get_ead_resource_description_from_local_fixture method' do
+      expect(subject).to respond_to(:get_ead_resource_description_from_local_fixture).with(2).arguments
+    end
+
+    # http://archivesspace.github.io/archivesspace/api/#authentication
     it 'has #authenticate method' do
       # subject.authenticate
       expect(subject).to respond_to(:authenticate).with(0).arguments
