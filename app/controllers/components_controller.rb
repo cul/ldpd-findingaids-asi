7a8268303ed83546941745f7eae9eff6534a3602
Fred Duby <fcd1@columbia.edu>
2019-04-25 17:24:23 -0400

    component index view, and bug fix for nested components

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -5,9 +5,22 @@ require 'archive_space/ead/ead_component_parser'
 class ComponentsController < ApplicationController
   before_action :set_bib_id,
                 :validate_repository_code_and_set_repo_id,
-                :validate_bib_id_and_set_resource_id,
+                :validate_bib_id_and_set_resource_id
 
   def index
+    if CONFIG[:use_fixtures]
+      @input_xml =
+        @as_api.get_ead_resource_description_from_local_fixture(@as_repo_id,@as_resource_id)
+    else
+      @input_xml = @as_api.get_ead_resource_description(@as_repo_id,@as_resource_id)
+    end
+    @ead = ArchiveSpace::Ead::EadParser.new @input_xml
+    @series_titles = @ead.archive_dsc_series_titles
+    @flattened_component_structure_array = []
+    @series_titles.each_with_index do |title, index|
+      ead_series_set_properties(index + 1)
+      @flattened_component_structure_array.append @flattened_component_structure
+    end
   end
 
   def show
@@ -17,13 +30,13 @@ class ComponentsController < ApplicationController
     else
       @input_xml = @as_api.get_ead_resource_description(@as_repo_id,@as_resource_id)
     end
+    @ead = ArchiveSpace::Ead::EadParser.new @input_xml
+    @series_titles = @ead.archive_dsc_series_titles
     ead_series_set_properties params[:id]
   end
 
   private
   def ead_series_set_properties component_num
-    @ead = ArchiveSpace::Ead::EadParser.new @input_xml
-    @series_titles = @ead.archive_dsc_series_titles
     component_nokogiri_xml = @ead.archive_dsc_series[component_num.to_i - 1]
     @component = ArchiveSpace::Ead::EadComponentParser.new
     @component.parse component_nokogiri_xml
