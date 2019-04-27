5841772f6e25b5017476146f562213ae9892c9f7
Fred Duby <fcd1@columbia.edu>
2019-04-26 23:59:40 -0400

    Add helper method to convert <title render="italic"> to <i>

--- app/helpers/finding_aids_helper.rb
+++ app/helpers/finding_aids_helper.rb
@@ -1,2 +1,9 @@
 module FindingAidsHelper
+  def apply_title_render_italic content
+    titles_render_italic = content.xpath('./xmlns:title[@render="italic"]')
+    titles_render_italic.each do |title_italic|
+      title_italic.replace "<i>#{title_italic.text}</i>"
+    end
+    content
+  end
 end
--- spec/helpers/finding_aids_helper_spec.rb
+++ spec/helpers/finding_aids_helper_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
 # the FindingAidsHelper. For example:
@@ -10,6 +11,27 @@ require 'rails_helper'
 #     end
 #   end
 # end
+
 RSpec.describe FindingAidsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'API/interface' do
+    context 'has' do
+      it 'apply_title_render_italics method taking one argument' do
+        expect(helper).to respond_to(:apply_title_render_italic).with(1).arguments
+      end
+    end
+  end
+
+  describe 'Functionality' do
+    context '#apply_title_render_italic' do
+      it 'italicizes content within <title render="italic">' do
+        xml_input = fixture_file_upload('asi/title_render_italic.xml').read
+        # Note: bogus ead format, would never have <p> as direct child of <ead>
+        # but not an issue here, just testing the italicizing functionality
+        # and just want a simple xml doc for Nokogiri to parse.
+        nokogiri_xml = Nokogiri::XML(xml_input).xpath('/xmlns:ead/xmlns:p')
+        result = helper.apply_title_render_italic nokogiri_xml
+        expect(result.to_s).to include('<i>The Decameron</i>')
+      end
+    end
+  end
 end
