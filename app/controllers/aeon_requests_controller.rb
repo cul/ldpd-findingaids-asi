6900648db3733ea0c2c2f28c3570cd480af083c5
Fred Duby <fcd1@columbia.edu>
2019-05-28 16:14:56 -0400

    Handle note and date scheduled in aeon request

--- app/controllers/aeon_requests_controller.rb
+++ app/controllers/aeon_requests_controller.rb
@@ -16,17 +16,23 @@ class AeonRequestsController < ApplicationController
     session[:selected_containers] = @selected_containers.to_a
     session[:bib_id] = params[:bib_id]
     session[:item_title] = params[:item_title]
+    session[:notes] = params[:notes]
+    session[:scheduled_date] = Date.strptime(params[:scheduled_date],'%Y-%m-%d').strftime('%m/%d/%Y')
   end
 
   def redirectshib
     @selected_containers = session[:selected_containers]
     @bib_id = session[:bib_id]
     @item_title = session[:item_title]
+    @notes = session[:notes]
+    @scheduled_date = session[:scheduled_date]
   end
 
   def redirectnonshib
     @selected_containers = session[:selected_containers]
     @bib_id = session[:bib_id]
     @item_title = session[:item_title]
+    @notes = session[:notes]
+    @scheduled_date = session[:scheduled_date]
   end
 end
