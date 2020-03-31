b7e1e2fa9e0d30d00423099cf203ad5f204402f8
Fred Duby <fcd1@columbia.edu>
2020-03-31 13:49:32 -0400

    Change default ask for prod deploy

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,3 +1,5 @@
 server 'ldpd-nginx-prod1.cul.columbia.edu', user: fetch(:remote_user), roles: %w(app db web)
 # Current branch is suggested by default in development
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# fcd1, 03/31/20: Change deploy default from current branch (I believe) to latest tag
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
