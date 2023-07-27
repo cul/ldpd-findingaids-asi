c75e3b1736c3950d3abea1da0bcde96f3c8e2c58
Benjamin Armintor <armintor@gmail.com>
2023-07-27 11:50:06 -0400

    install Blacklight - use import map for js and sprockets for css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  mount Blacklight::Engine => '/'
+  concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+  devise_for :users
+
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
   get 'pages/home'
   get 'resolve/:id', to: 'finding_aids#resolve'
   scope 'ead' do
