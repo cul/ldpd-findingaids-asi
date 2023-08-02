8979c3338d81cfdf6a32d3a23ee556fb09790a8d
Benjamin Armintor <armintor@gmail.com>
2023-08-02 14:22:52 -0400

    allow aliases when we parse YAML in Arclight::Repository

--- config/initializers/arclight_patch.rb
+++ config/initializers/arclight_patch.rb
@@ -1,10 +1,14 @@
 require 'arclight'
 
 class Arclight::Repository
+  def id
+    self.slug
+  end
+
   # Override to pull repository configs from an environment keyed top-level hash
   def self.from_yaml(file)
     repos = {}
-    data = YAML.safe_load(File.read(file))[Rails.env]
+    data = YAML.safe_load(File.read(file), aliases: true)[Rails.env]
     data.each_key do |slug|
       repos[slug] = new(data[slug].merge(slug: slug))
     end
