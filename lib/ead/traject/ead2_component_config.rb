0f91b938620ddd50d26f0a5de840a3842a55f71a
Benjamin Armintor <armintor@gmail.com>
2023-08-04 17:43:18 -0400

    update traject indexers to include repository id

---
+++ lib/ead/traject/ead2_component_config.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'traject'
+
+# rubocop:disable Style/MixinUsage
+extend TrajectPlus::Macros
+# rubocop:enable Style/MixinUsage
+
+load_config_file "#{Arclight::Engine.root}/lib/arclight/traject/ead2_component_config.rb"
+
+settings do
+  # provide 'root' # the root EAD collection indexing context
+  # provide 'parent' # the immediate parent component (or collection) indexing context
+  # provide 'counter' # a global component counter to provide a global sort order for nested components
+  # provide 'depth' # the current nesting depth of the component
+  provide 'component_traject_config', __FILE__
+end
+
+to_field 'repository_id_ssi' do |record, accumulator, context|
+  if settings[:root]&.clipboard
+  	repository_id = settings[:root].clipboard[:repository_id]
+    accumulator.concat([repository_id]) if repository_id
+  end
+end
