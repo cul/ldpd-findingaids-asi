e41c3488b6575f2415bddb4a96893869defcf3d3
Benjamin Armintor <armintor@gmail.com>
2023-10-09 10:55:22 -0400

    Implement lweb-like UI (ACFA-427) - continue replacing local Repository model with Arclight::Repository (ACFA-407)

--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe RepositoriesController, type: :controller do
 
   describe "GET #index" do
+    before do
+      allow(controller).to receive(:load_collection_counts).and_return({})
+    end
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
