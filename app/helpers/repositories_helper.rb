19858bbaa3edf5b19d937db7bf5593b4e5db8087
Benjamin Armintor <armintor@gmail.com>
2023-10-17 17:12:33 -0400

    link repository facet to code and translate label (ACFA-424)

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,2 +1,15 @@
 module RepositoriesHelper
+  def repository_label(code_value)
+    Arclight::Repository.find_by(slug: code_value)&.name || code_value
+  end
+
+  # Override of ArclightHelper#repository_collections_path
+  def repository_collections_path(repository)
+    search_action_url(
+      f: {
+        repository: [repository.slug],
+        level: ['Collection']
+      }
+    )
+  end
 end
