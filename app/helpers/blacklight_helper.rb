f80e56f69e0934a65c1697580db94a23a71f8358
Benjamin Armintor <armintor@gmail.com>
2023-10-18 15:33:55 -0400

    do not session track external links in search results (ACFA-431)

---
+++ app/helpers/blacklight_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module BlacklightHelper
+  include Blacklight::BlacklightHelperBehavior
+
+  # Override Blacklight helper to prevent session tracking external links
+  def link_to_document(doc, field_or_opts = nil, opts = { counter: nil })
+    label = case field_or_opts
+            when NilClass
+              document_presenter(doc).heading
+            when Hash
+              opts = field_or_opts
+              document_presenter(doc).heading
+            else # String
+              field_or_opts
+            end
+    doc_link = search_state.url_for_document(doc)
+    link_html_opts = doc_link.is_a?(Hash) ? document_link_params(doc, opts) : { target: '_blank', class: "external-link", rel: "noopener noreferrer" }
+    link_to label, doc_link, link_html_opts
+  end
+end
