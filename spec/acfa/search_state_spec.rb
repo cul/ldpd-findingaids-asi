0303169dad8b4eb19998c62bff9e754e3feddfc1
Benjamin Armintor <armintor@gmail.com>
2023-10-19 17:18:16 -0400

    remove routing overrides for legacy views

--- spec/acfa/search_state_spec.rb
+++ spec/acfa/search_state_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Acfa::SearchState do
   context "repo has no aspace_base_uri configured" do
     let(:expected) { { repository_id: repository_id, finding_aid_id: finding_aid_id, controller: 'components', action: 'index', anchor: 'view_all' } }
     it 'fails' do
-      expect(actual).to eql(expected)
+      expect(actual).to be(document)
     end
   end
   context "repo has aspace_base_uri configured" do
