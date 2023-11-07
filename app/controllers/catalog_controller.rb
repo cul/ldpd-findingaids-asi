0362150fa7157b7ff5ec55b5dad6a6c04380745a
Benjamin Armintor <armintor@gmail.com>
2023-11-07 09:00:49 -0500

    correct Blacklight 8 compatibility issues in hierarchy action - paginate param permitted - sanitized search params remove id, so it must be fetched from controller params

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -245,7 +245,7 @@ class CatalogController < ApplicationController
     end
 
     # These are the parameters passed through in search_state.params_for_search
-    config.search_state_fields += %i[id group hierarchy_context original_document]
+    config.search_state_fields += %i[id group hierarchy_context original_document paginate]
     config.search_state_fields << { original_parents: [] }
     config.search_state_fields += %i[repository_id finding_aid_id]
     config.search_state_fields << :utf8
--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -5,6 +5,17 @@ class SearchBuilder < Blacklight::SearchBuilder
 
   include Arclight::SearchBehavior
 
+  def add_hierarchy_behavior(solr_parameters)
+    return unless search_state.controller&.action_name == 'hierarchy'
+    request_id = search_state.controller.params[:id]
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << "_nest_parent_:#{blacklight_params[:id] || request_id}"
+    solr_parameters[:rows] = blacklight_params[:per_page]&.to_i || blacklight_params[:limit]&.to_i || 999_999_999
+    solr_parameters[:start] = blacklight_params[:offset] if blacklight_params[:offset]
+    solr_parameters[:sort] = 'sort_isi asc'
+    solr_parameters[:facet] = false
+    #solr_parameters[:group] = false # explicitly ungroup in case it was defaulted
+  end
   ##
   # @example Adding a new step to the processor chain
   #   self.default_processor_chain += [:add_custom_data_to_query]
