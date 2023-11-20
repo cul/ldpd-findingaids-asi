3cc5cfeb3fd966eedaa4cd62580112202799c4af
Benjamin Armintor <armintor@gmail.com>
2023-11-20 10:47:24 -0500

    Repositry links from homepage lead to pinned search (ACFA-458)

--- app/components/acfa/search_bar_component.rb
+++ app/components/acfa/search_bar_component.rb
@@ -9,5 +9,13 @@ module Acfa
       (@kwargs[:form_options][:html] ||=  {})[:id] = 'hero-search-form'
       (@kwargs[:classes] ||= ['search-query-form']) << 'd-flex'
     end
+
+    def params_for_search
+      use_params = @params.merge(f: (@params[:f] || {}).except(:collection)).except(:repository_id)
+      if @params[:repository_id]
+        use_params[:repository] ||= @params[:repository_id]
+      end
+      use_params
+    end
   end
 end
