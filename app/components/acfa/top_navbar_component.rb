34ada1a52a14cc1a2faa43d0563bb491da2cd6c9
Benjamin Armintor <armintor@gmail.com>
2023-11-29 18:06:37 -0500

    compact titles on small displays, CUL svg logo (WEBDEV-765)

--- app/components/acfa/top_navbar_component.rb
+++ app/components/acfa/top_navbar_component.rb
@@ -3,7 +3,9 @@
 module Acfa
   class TopNavbarComponent < Blacklight::TopNavbarComponent
     def logo_link(title: application_name)
-      link_to title, blacklight_config.logo_link, class: 'mb-0 navbar-brand navbar-logo'
+      link_to(blacklight_config.logo_link, class: 'mb-0 navbar-brand navbar-logo') do
+        'Archival Collections<span class="d-none d-s-inline"> at Columbia University</span>'.html_safe
+      end
     end
   end
 end
