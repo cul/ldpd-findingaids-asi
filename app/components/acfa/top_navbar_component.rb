e6bfe9df7dc8dd5cb6ba8736bdca8fd7d4414806
Benjamin Armintor <armintor@gmail.com>
2023-11-30 15:23:29 -0500

    do not display CUL branding and also name organization in page header (ACFA-775)

--- app/components/acfa/top_navbar_component.rb
+++ app/components/acfa/top_navbar_component.rb
@@ -3,9 +3,7 @@
 module Acfa
   class TopNavbarComponent < Blacklight::TopNavbarComponent
     def logo_link(title: application_name)
-      link_to(blacklight_config.logo_link, class: 'mb-0 navbar-brand navbar-logo') do
-        'Archival Collections<span class="d-none d-s-inline"> at Columbia University</span>'.html_safe
-      end
+      link_to('Archival Collections', blacklight_config.logo_link, class: 'mb-0 navbar-brand navbar-logo')
     end
   end
 end
