4c43111cc1879b6b66b7c29c9fef66f8db9b70f2
Bonnie Gordon <blg2144@columbia.edu>
2024-03-20 17:30:50 -0400

    start to override collection box

---
+++ app/components/acfa/arclight/collection_context_component.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Acfa::Arclight
+  # Render various actions for a collection (e.g. requesting, download links, etc)
+  class CollectionContextComponent < ViewComponent::Base
+    def initialize(presenter:, download_component:)
+      super
+
+      @collection = presenter.document.collection
+      @download_component = download_component
+    end
+
+    attr_reader :collection
+
+    def title
+      collection.normalized_title
+    end
+
+    def document_download
+      render @download_component.new(downloads: collection.downloads) || Arclight::DocumentDownloadComponent.new(downloads: collection.downloads)
+    end
+
+    def collection_info
+      render Arclight::CollectionInfoComponent.new(collection: collection)
+    end
+  end
+end
