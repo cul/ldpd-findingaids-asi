87bb7d83cc0749248e319e731d9b7d834828827a
Bonnie Gordon <blg2144@columbia.edu>
2024-03-26 09:33:26 -0400

    remove bookmark checkbox

---
+++ app/components/acfa/arclight/document_component.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Acfa::Arclight
+  # Render a single document
+  class DocumentComponent < Arclight::DocumentComponent
+    attr_reader :document
+
+    def online_content?
+      document.online_content? && (document.collection? || document.children?)
+    end
+
+    def blacklight_config
+      presenter.configuration
+    end
+
+    # @return [Blacklight::Configuration::ToolConfig] the configuration for the bookmark
+    def bookmark_config
+      blacklight_config.index.document_actions.arclight_bookmark_control
+    end
+
+    def breadcrumb_component
+      blacklight_config.show.breadcrumb_component || Arclight::BreadcrumbsHierarchyComponent
+    end
+
+    def metadata_partials
+      blacklight_config.show.metadata_partials || []
+    end
+
+    def component_metadata_partials
+      blacklight_config.show.component_metadata_partials || []
+    end
+
+    def online_filter
+      render Arclight::OnlineContentFilterComponent.new(document: document)
+    end
+    
+
+
+    def access
+      render (blacklight_config.show.access_component || Arclight::AccessComponent).new(presenter: presenter)
+    end
+
+    def toggle_sidebar
+      button_tag(t('arclight.views.show.toggle_sidebar'),
+                 type: :button,
+                 class: 'btn btn-sm btn-secondary d-lg-none sidebar-toggle',
+                 data: { bs_toggle: 'offcanvas', bs_target: '#sidebar' },
+                 aria: { controls: 'sidebar' })
+    end
+  end
+end
