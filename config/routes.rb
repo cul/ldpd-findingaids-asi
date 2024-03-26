f2fa02f039d5e51cace09fc0f42cf6714a4991d7
Bonnie Gordon <blg2144@columbia.edu>
2024-03-26 13:12:20 -0400

    remove unused preview routes

--- config/routes.rb
+++ config/routes.rb
@@ -40,16 +40,7 @@ Rails.application.routes.draw do
       end
     end
   end
-  scope 'preview' do
-    scope 'ead' do
-      resources :repositories, only: [:index], path: '' do
-        resources :finding_aids, only: [:index, :show], path: '' do
-          get 'summary'
-          resources :components, only: [:index, :show], path: 'dsc'
-        end
-      end
-    end
-  end
+
   resource :aeon_request, only: [:create] do
     post 'login'
     get 'redirectshib'
