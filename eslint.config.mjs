bf1143864b28a4629c60a1bcfeef4a353343d659
Eric O <eric@ejdigitalmedia.com>
2024-04-17 13:48:07 -0400

    Change eslint package.json script scope

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,12 +17,9 @@ export default [
   },
   {
     ignores: [
-      'eslint.config.mjs',
       // For now, ignoring some Rails js files that might need to be reworked as part of the Vite changes
       'app/javascript/application.js',
       'app/javascript/controllers/*',
-      // Temporarily ignoring aeonform.js while we figure out what to do with that file
-      'app/assets/javascripts/aeonform.js',
     ]
   },
   { languageOptions: { globals: globals.browser } },
