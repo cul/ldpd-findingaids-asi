78eb61dabed01eb1edc16aa23d48ed359f94d519
Eric O <eric@ejdigitalmedia.com>
2024-04-17 13:14:51 -0400

    Update eslint config and fix some linter errors

--- app/javascript/components/RequestCart.jsx
+++ app/javascript/components/RequestCart.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const RequestCart = () => {
+function RequestCart() {
   return (
     <div>
       <h1>Cart</h1>
       <p>Cart stuff goes here!</p>
     </div>
   );
-};
+}
 
 export default RequestCart;
