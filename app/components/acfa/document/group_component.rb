6964fee9abfb08d79ac394502ef84907c22a2f89
Bonnie Gordon <blg2144@columbia.edu>
2024-04-17 16:22:02 -0400

    fixes ACFA-503
    remove “no more results” override for collection result in grouped by collection

--- app/components/acfa/document/group_component.rb
+++ app/components/acfa/document/group_component.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 
-class Acfa::Document::GroupComponent < ::Arclight::GroupComponent
-
-  def grouped_documents
-    @grouped_docs ||= @group.docs.select { |doc| doc.id != @group.key}.slice(0, 3)
-  end
-end
+class Acfa::Document::GroupComponent < ::Arclight::GroupComponent; end
