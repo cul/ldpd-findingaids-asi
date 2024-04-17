b90c254907894593f3c1dd1b9c98c73403752387
Eric O <eric@ejdigitalmedia.com>
2024-04-17 14:50:19 -0400

    Dowgrade from ESLint 9 to ESLint 8 (and use .eslintrc.js instead of eslint.config.mjs flat config, for better compatibility with other dependencies)

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    // For now, ignoring some Rails js files that might need to be reworked as part of the Vite changes
+    'app/javascript/application.js',
+    'app/javascript/controllers/*',
+  ],
+  rules: {
+    'max-len': ['error', { code: 120 }],
+  },
+};
