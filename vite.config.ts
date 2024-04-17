e805a82e4dc53e6ba7d496dc315a30b755a744b2
Eric O <eric@ejdigitalmedia.com>
2024-04-17 11:49:19 -0400

    ESLint setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    eslint()
   ],
 })
