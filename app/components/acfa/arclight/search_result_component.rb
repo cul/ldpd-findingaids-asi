b0c4d8b5fe06f30dda9666e3ce327af0bbc57309
Bonnie Gordon <blg2144@columbia.edu>
2024-04-29 12:32:07 -0400

    remove container info from component search result

---
+++ app/components/acfa/arclight/search_result_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Acfa::Arclight
+  # Render a document for a search result; this works with
+  # both the compact and list views for grouped or ungrouped
+  # results.
+  class SearchResultComponent < Arclight::SearchResultComponent
+  end
+end
---
+++ app/components/acfa/arclight/search_result_title_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Acfa::Arclight
+  # Render a document title for a search result
+  class SearchResultTitleComponent < Arclight::SearchResultTitleComponent
+  end
+end
