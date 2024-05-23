0ce522666dee5cb21540ab4c21afaa52fd196a37
Eric O <eric@ejdigitalmedia.com>
2024-05-23 15:12:30 -0400

    Use Acfa::Index.build_suggester method in DeleteEadJob#perform; Add test for DeleteEadJob

--- app/jobs/delete_ead_job.rb
+++ app/jobs/delete_ead_job.rb
@@ -1,12 +1,10 @@
 class DeleteEadJob < ApplicationJob
-  
   def perform(bibid)
     solr_con = Blacklight.default_index.connection
-    solr_response = solr_con.delete_by_query "_root_:#{bibid}"
+    solr_con.delete_by_query "_root_:#{bibid}"
     solr_con.commit(softCommit: true)
     solr_url = solr_con.base_uri
-    `curl #{solr_url}suggest?suggest.build=true`
+    Acfa::Index.build_suggester(solr_url)
     1
   end
-  
 end
