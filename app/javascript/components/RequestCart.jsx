ea740909f63aa991f0a9fc738f4cc2f1bcddf431
Eric O <elohanlon@users.noreply.github.com>
2024-05-23 16:28:57 -0400

    Merge WIP ACFA-490 branch into dev-v2.0.0 (to sync up vite and other early cart setup stuff) (#88)
    * start to override collection box
    
    * add aeon request button
    
    * Added Vite to repo, import_map still present due to dependency within BlackLight
    
    * Added react entrypoint (react dependencies incoming)
    
    * Added React dependencies
    
    * removed lingering checkboxes code and aeon submit in application.js from previous request implementation
    
    * Removed importmap gem in favor of modifying and including a custom blacklight/base.html.erb. All Vite errors resolved.
    
    * Removed remaining references to importmap
    
    * Added missing vite_client_tag to blacklight/base.html.erb, added vite instructions to README
    
    * Configured the app to use Node version 20.10 and added node version manager
    instructions
    
    * Updated README with yarn installation instruction
    
    * Added instructions for nvm version switching
    
    * Fixed issue with Collection Contents never loading
    
    * Removed extra line after turbo import
    
    * Re-added vite tags to blacklight.html.erb after they were accidentally removed
    
    * Added @github/auto-complete-element to package.json to fix autocomplete showing on homepage
    
    * Removed placeholder text in cart area
    
    * ESLint setup
    
    * Update ci.yml to add Node 20 + yarn, and run js linter
    
    * Update eslint config and fix some linter errors
    
    * Change eslint package.json script scope
    
    * Dowgrade from ESLint 9 to ESLint 8 (and use .eslintrc.js instead of eslint.config.mjs flat config, for better compatibility with other dependencies)
    
    * Added aeonform.js to ignored files for eslint
    
    * Added item list display to cart, and list of object to store cart items
    
    * Implemented localStorage, conents showed in cart list
    
    * Update eslint config and fix some linter errors
    
    * Dowgrade from ESLint 9 to ESLint 8 (and use .eslintrc.js instead of eslint.config.mjs flat config, for better compatibility with other dependencies)
    
    * Fixed some linter issues with RequestCart component
    
    * WIP adding cart as toggle-able sidebar
    
    * Added react-bootstrap to repo
    
    * Setup initial coffcanvas cart
    
    * Added cart sidebar as bootstrap Offcanvas
    
    * Update yarn.lock and delete unused package-lock.json
    
    * For now, hide work-in-progress cart functionality in UI
    
    * Fix some js issues by adding missing npm dependencies
    
    * Remove duplicate vite tags
    
    * eslint configuration updates
    
    * Set up cart-widget in footer, and enable global triggering of cart display
    
    * Removed a few component overrides that we don't need right now
    
    * Fixes for React app and Turbo compatibility after page navigation
    
    * Restore deleted access method call in app/components/acfa/arclight/document_component.html.erb
    
    * Remove some unused js
    
    ---------
    
    Co-authored-by: Bonnie Gordon <blg2144@columbia.edu>
    Co-authored-by: Nathan Alexander Walker <nwalker2398@yahoo.com>

---
+++ app/javascript/components/RequestCart.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+// import removeFromCart from '../../assets/javascripts/cart.js';
+
+// const makeCartItem = (itemName, collection, readingRoom) => ({
+//   name: itemName,
+//   collection,
+//   readingRoom,
+// });
+
+// const cart = [
+//   makeCartItem('Item 1', 'Collection 1', 'Room 1'),
+//   makeCartItem('Item 2', 'Collection 2', 'Room 2'),
+//   makeCartItem('Item 3', 'Collection 3', 'Room 3'),
+//   makeCartItem('Item 4', 'Collection 4', 'Room 4'),
+//   makeCartItem('Item 5', 'Collection 5', 'Room 5'),
+//   makeCartItem('Item 6', 'Collection 6', 'Room 6')
+// ]
+
+const cart = JSON.parse(localStorage.getItem('cart'));
+
+function removeFromCart(toRemove) {
+  const newCart = JSON.parse(localStorage.getItem('cart'));
+  newCart.filter((item) => (item.name !== toRemove.name || item.collection !== toRemove.collection));
+  localStorage.setItem('cart', JSON.stringify(newCart));
+}
+
+function RequestCart() {
+  const [show, setShow] = useState(false);
+
+  const hideCart = () => setShow(false);
+  const showCart = () => setShow(true);
+
+  useEffect(() => {
+    window.addEventListener('showCart', showCart);
+    return () => {
+      window.removeEventListener('showCart', showCart);
+    };
+  });
+
+  return (
+    <div className="request-cart">
+      <Offcanvas className="w-50" show={show} onHide={hideCart} placement="end" scroll>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Request Cart</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Table>
+            <thead>
+              <tr>
+                <th>Item Name</th>
+                <th>Collection Name</th>
+                <th>Reading Room Location</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((cartItem) => (
+                <tr key={cartItem.name}>
+                  <td>{cartItem.name}</td>
+                  <td>{cartItem.collection}</td>
+                  <td>{cartItem.readingRoom}</td>
+                  <td>
+                    <Button onClick={removeFromCart(cartItem)}>
+                      Remove
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
+  );
+}
+
+export default RequestCart;
