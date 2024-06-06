72c01c6721fc75cf8e7976f7e111e3922fc0c26e
Bonnie <bonniegee@users.noreply.github.com>
2024-06-06 09:38:21 -0400

    Adjust styling and layout on search results page (#100)
    * fixes ACFA-511
    
    adjust wording that indicates # of results in a collection in "group by collection" view
    
    * fixes ACFA-503
    
    remove “no more results” override for collection result in grouped by collection
    
    * Adjust visual spacing on "Grouped by collection" results (#86)
    
    Fixes ACFA-512
    
    * remove container info from component search result
    
    * change result limit to 3
    
    * move number of results to top box
    
    * style changes to group by collection view
    
    * update style on all results page
    
    * remove duplicate css

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,6 @@ module LdpdFindingaidsAsi
         load override
       end
     end
-    Arclight::Engine.config.catalog_controller_group_query_params['group.limit'] = 4
+    Arclight::Engine.config.catalog_controller_group_query_params['group.limit'] = 3
   end
 end
