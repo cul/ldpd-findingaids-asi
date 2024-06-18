a411a27df2ee32fd2b43a8c2fd5e728821f59cb2
JackBlacklight <jd2148@columbia.edu>
2024-06-18 14:25:51 -0400

    set default host in environments config (ACFA-577)

--- config/environments/findingaids_dev.rb
+++ config/environments/findingaids_dev.rb
@@ -5,4 +5,7 @@ Rails.application.configure do
 
   # Setting host so that url helpers can be used in mailer views.
   config.action_mailer.default_url_options = { host: 'https://findingaids-dev.library.columbia.edu'}
+
+  #set default host for sitemap generator
+  config.default_host = "https://findingaids-dev.library.columbia.edu"
 end
