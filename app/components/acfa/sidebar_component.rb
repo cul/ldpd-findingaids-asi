d929b5d82108f02c5803b988e6497f28cdc86f16
Bonnie Gordon <blg2144@columbia.edu>
2024-07-12 14:43:46 -0400

    WIP add functioning search

--- app/components/acfa/sidebar_component.rb
+++ app/components/acfa/sidebar_component.rb
@@ -3,5 +3,13 @@
 module Acfa
   # A sidebar with collection context widget and tools
   class SidebarComponent < ::Arclight::SidebarComponent
+    def collection_name
+      @collection_name ||= Array(controller.params.dig(:f, :collection)).reject(&:empty?).first ||
+                           helpers.current_context_document&.collection_name
+    end
+    
+    def repository_id
+      controller.params[:repository_id]
+    end
   end
 end
