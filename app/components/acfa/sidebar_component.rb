28c5fadc19e14329b1ff97a6ae919d759cd22cb9
Bonnie Gordon <blg2144@columbia.edu>
2024-07-12 15:08:55 -0400

    fixes ACFA-529
    Add collection-specific search bar to "Collection contents" sidebar

--- app/components/acfa/sidebar_component.rb
+++ app/components/acfa/sidebar_component.rb
@@ -11,5 +11,13 @@ module Acfa
     def repository_id
       controller.params[:repository_id]
     end
+    
+    def render_search_bar
+      sb = Acfa::SearchBarComponent.new(url: helpers.search_action_url, params: {repository_id: repository_id, f: {collection: [collection_name]}})
+      sb.with_search_button do
+        '<button class="btn btn-primary search-btn" type="submit" id="search"><span class="visually-hidden-sm me-sm-1 submit-search-text">Search</span><i class="fa-solid fa-magnifying-glass-location"></i></button>'.html_safe
+      end
+      render sb
+    end
   end
 end
