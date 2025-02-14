4a8abed4fee246a9da1f5e55a594546a3c72bf8f
Bonnie Gordon <blg2144@columbia.edu>
2025-02-14 14:23:08 -0500

    WIP ACFA-592
    create basic admin section with working refresh form front end

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,7 +5,6 @@ class AdminController < ApplicationController
   
   # POST /admin/refresh_resource
   def refresh_resource
-    sleep 2
     render json: {result: true, resource_id: 'cul-13202800'}
   end
 end
