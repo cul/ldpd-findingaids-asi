4f211709302235305bdf7c287d762b4affb56ea3
Bonnie Gordon <blg2144@columbia.edu>
2025-02-14 14:23:08 -0500

    WIP ACFA-592
    create basic admin section with working refresh form front end

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -1,25 +1,51 @@
-const csrfParam = document.querySelector('meta[name="csrf-param"]');
-const csrfToken = document.querySelector('meta[name="csrf-token"]');
+const csrfParam = document.querySelector('meta[name="csrf-param"]').content;
+const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+const refreshResultStatusElement = document.getElementById('refresh-result-status');
 
 const refreshResourceForm = document.getElementById('refresh-resource-form');
+
 if (refreshResourceForm) {
-  refreshResourceForm.addEventListener('submit', (e) => {
+  refreshResourceForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const { action, method } = e.target;
-    const resourceRecordUri = document.getElementById('resource_record_uri');
-    const includeUnpublished = document.getElementById('include_unpublished');
+    const resourceRecordUri = document.getElementById('resource_record_uri').value;
+    const includeUnpublished = document.getElementById('include_unpublished').value === 'true';
 
-    fetch(action, {
-      method,
-      // credentials: 'include', // TODO: Might uncomment this later if needed
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        [csrfParam]: csrfToken,
-        resource_record_uri: resourceRecordUri,
-        include_unpublished: includeUnpublished,
-      }),
-    });
+    refreshResultStatusElement.innerHTML = 'Loading...';
+
+    try {
+      const response = await fetch(action, {
+        method,
+        // credentials: 'include', // TODO: Might uncomment this later if needed
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          [csrfParam]: csrfToken,
+          resource_record_uri: resourceRecordUri,
+          include_unpublished: includeUnpublished,
+        }),
+      });
+
+      if (response.status !== 200) {
+        throw new Error(`Received a response status of ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      if (data.result) {
+        refreshResultStatusElement.innerHTML = `
+          <p class="alert alert-success mt-3">
+            Collection reindexed successfully!
+            <a href="/archives/${data.resource_id}">View this collection &raquo;</a>
+          </p>
+        `;
+      } else {
+        throw new Error(data.error);
+      }
+    } catch (err) {
+      console.log(err);
+      refreshResultStatusElement.innerHTML = `<p class="alert alert-danger mt-3"><strong>An unexpected error occurred</strong>:<br />${err}</p>`;
+    }
   });
 }
