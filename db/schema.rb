29928a205ab32da17bfdf10cb2a679bd98b9b77a
Eric O <eric@ejdigitalmedia.com>
2025-02-28 16:50:55 -0500

    Set up CAS authentication using omniauth-cul gem; Set up sign in and sign out UI elements; Override some Devise partials; Add permissions.template.yml file; Update deployment symlinks to include permissions.yml; Update application layout so that it now displays flash messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_153137) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_24_221444) do
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "user_type"
@@ -41,8 +41,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_153137) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "guest", default: false
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
 end
