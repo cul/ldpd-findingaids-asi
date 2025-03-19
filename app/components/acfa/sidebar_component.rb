c4f4d6b1e848a3c9afe96b11fe62780985a1631d
Eric O <eric@ejdigitalmedia.com>
2025-03-19 14:49:30 -0400

    Test: Commenting out code in blacklight-shims.scss

--- app/components/acfa/sidebar_component.rb
+++ app/components/acfa/sidebar_component.rb
@@ -7,15 +7,15 @@ module Acfa
       @collection_name ||= Array(controller.params.dig(:f, :collection)).reject(&:empty?).first ||
                            helpers.current_context_document&.collection_name
     end
-    
+
     def repository_id
       controller.params[:repository_id]
     end
-    
+
     def render_search_bar
       sb = Acfa::SearchBarComponent.new(url: helpers.search_action_url, params: {repository_id: repository_id, f: {collection: [collection_name]}})
       sb.with_search_button do
-        '<button class="btn btn-primary search-btn align-items-center" type="submit" id="search"><span class="d-none d-md-inline me-sm-1 submit-search-text">Search</span><i class="fa-solid fa-lg fa-folder-magnifying-glass"></i></button>'.html_safe
+        '<button class="btn btn-primary search-btn align-items-center" type="submit" id="search"><span class="d-none d-md-inline me-sm-1 submit-search-text visually-hidden">Search</span><i class="fa-solid fa-lg fa-folder-magnifying-glass"></i></button>'.html_safe
       end
       render sb
     end
