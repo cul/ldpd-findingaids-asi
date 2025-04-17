7badc4bc9b0a0771c8c92adcf719419496cdf938
Benjamin Armintor <armintor@gmail.com>
2025-04-16 22:43:56 -0400

    null safety in app/javascript/blacklight-range-limit.js

--- app/javascript/blacklight-range-limit.js
+++ app/javascript/blacklight-range-limit.js
@@ -3,10 +3,12 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Remove .sr-only on this element because FontAwesome defines an .sr-only class and we don't want it to apply here
-  document.querySelector(
+  const inputSelector = document.querySelector(
     '.blacklight-date_range .range-limit-input-group input.btn.sr-only',
-  ).classList.remove('sr-only');
+  );
+  if (inputSelector) inputSelector.classList.remove('sr-only');
 
   // Hide .more_facets
-  document.querySelector('.blacklight-date_range .more_facets').classList.add('visually-hidden');
+  const facetSelector = document.querySelector('.blacklight-date_range .more_facets');
+  if (facetSelector) facetSelector.classList.add('visually-hidden');
 });
