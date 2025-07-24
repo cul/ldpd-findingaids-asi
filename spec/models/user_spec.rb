6e8e5673dd22e9ca501615c4787534ea6edacd2b
Bonnie Gordon <blg2144@columbia.edu>
2025-07-24 11:17:16 -0400

    add basic tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Devise module inclusions" do
+    it "includes :database_authenticatable" do
+      expect(User.devise_modules).to include(:database_authenticatable)
+    end
+
+    it "includes :validatable" do
+      expect(User.devise_modules).to include(:validatable)
+    end
+
+    it "includes :timeoutable" do
+      expect(User.devise_modules).to include(:timeoutable)
+    end
+
+    it "includes :omniauthable" do
+      expect(User.devise_modules).to include(:omniauthable)
+    end
+
+    it "configures :cas as an omniauth_provider" do
+      expect(User.omniauth_providers).to include(:cas)
+    end
+  end
 end
