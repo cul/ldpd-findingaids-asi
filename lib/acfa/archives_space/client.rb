4a0fd0d0728489434896612cabeed9a7b51e2beb
Eric O <eric@ejdigitalmedia.com>
2025-08-07 15:18:24 -0400

    ACFA-623: Refresh Aspace token after expired login session

--- lib/acfa/archives_space/client.rb
+++ lib/acfa/archives_space/client.rb
@@ -14,6 +14,30 @@ class Acfa::ArchivesSpace::Client < ArchivesSpace::Client
     @instance
   end
 
+  # We are overriding the ArchivesSpace::Client so that if the request fails because your session has expired,
+  # we'll log in again and retry.
+  def request(method, path, options = {})
+    response = super
+
+    # If this request has not been retried already, and we received a response status of 412, this may be a case
+    # where our session token has expired.  We'll retry the request.
+    # NOTE: If Archivematica ever changes its API and returns a different status code,
+    # we'll need to update the check below.
+    if response.status_code == 412 && options[:retry].nil?
+      # Clear any existing token (this is important to do, so that the upcoming login request does not include a token)
+      self.token = nil
+
+      # Log in again to generate and store a new token
+      self.login
+
+      # Make original request again.  This time it will use the newly generated token.
+      # We'll alslo include a retry flag in the options, to protect against infinite recursive loops.
+      new_options = options.merge({retry: true})
+      response = self.request(method, path, new_options)
+    end
+    response
+  end
+
   # Splits the given ArchivesSpace resource_record_uri into its constituent repository_id and resource_id.
   # @return [Array] An array of length 2.  The first element is the repository_id and the second is the resource_id.
   def split_resource_record_uri(resource_record_uri)
@@ -28,6 +52,7 @@ class Acfa::ArchivesSpace::Client < ArchivesSpace::Client
   end
 
   def bib_id_for_resource(resource_record_uri:)
+    #self.login
     repository_id, resource_id = split_resource_record_uri(resource_record_uri)
     response = self.get("/repositories/#{repository_id}/resources/#{resource_id}")
     raise_error_if_unsuccessful_archivesspace_response!(__method__, response)
@@ -36,6 +61,7 @@ class Acfa::ArchivesSpace::Client < ArchivesSpace::Client
   end
 
   def download_ead(resource_record_uri:, filename:, include_unpublished:)
+    #self.login
     repository_id, resource_id = split_resource_record_uri(resource_record_uri)
     download_path = File.join(CONFIG[:ead_cache_dir], filename)
 
