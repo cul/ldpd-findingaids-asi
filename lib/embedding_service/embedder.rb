b8f05a47c5f17dfe1029b97bc1a94795747acb24
Eric O <eric@ejdigitalmedia.com>
2025-08-20 18:33:04 -0400

    Truncate text embedding source text to 512 characters

--- lib/embedding_service/embedder.rb
+++ lib/embedding_service/embedder.rb
@@ -9,7 +9,7 @@ module EmbeddingService
         # puts "emb:"
         # puts field_value[0, 2000]
         # request.body = "text=#{field_value}"
-        request.body = "text=#{field_value[0, 1000]}"
+        request.body = "text=#{field_value[0, 512]}"
         request['Content-Type'] = 'application/x-www-form-urlencoded'
 
         begin
