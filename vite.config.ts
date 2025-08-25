9b508442457212edc0657bdcd608bb97f80f4dcd
Weronika Tomaszewska <wt2392@columbia.edu>
2025-08-25 10:30:45 -0400

    fix merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import RubyPlugin from 'vite-plugin-ruby';
-import eslint from 'vite-plugin-eslint';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+
+const eslintPlugin = () => {
+  return {
+    name: 'vite:eslint',
+    async buildStart() {
+      const { ESLint } = await import('eslint');
+      const eslint = new ESLint();
+      const results = await eslint.lintFiles(['app/javascript/**/*.{js,jsx,ts,tsx}']);
+
+      await ESLint.outputFixes(results);
+
+      const formatter = await eslint.loadFormatter('stylish');
+      const resultText = formatter.format(results);
+      if (resultText) {
+        console.log(resultText);
+      }
+    }
+  };
+};
 
 export default defineConfig({
   plugins: [
@@ -9,6 +27,6 @@ export default defineConfig({
     // cert when the server is running under https.
     basicSsl({}),
     RubyPlugin(),
-    eslint(),
-  ]
+    eslintPlugin(),
+  ],
 });
