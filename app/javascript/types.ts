2a0c353221cb4903b1f66a2e666d17c63f6081ee
Weronika Tomaszewska <wt2392@columbia.edu>
2025-09-24 14:30:19 -0400

    ACFA-650 convert mirador file to typescript; removee prop types; move mirador types to types.ts

--- app/javascript/types.ts
+++ app/javascript/types.ts
@@ -1,13 +1,62 @@
-declare global {
-  interface Window {
-    showCart: () => void;
-    addToCart: (recordData: RequestCartItem) => void;
-  }
+export interface MiradorConfig {
+  id: string;
+  window: WindowConfig;
+  windows: MiradorWindow[];
+  workspace: {
+    showZoomControls: boolean;
+  };
+  workspaceControlPanel: {
+    enabled: boolean;
+  };
+  miradorDownloadPlugin: {
+    restrictDownloadOnSizeDefinition: boolean;
+  };
+  translations: {
+    en: {
+      openCompanionWindow_citation: string;
+    };
+  };
 }
 
+export interface ViewConfig {
+  defaultView: string;
+  views: Array<{
+    key: string;
+    behaviors?: string[];
+  }>;
+  allowTopCollectionButton?: boolean;
+}
 
-export interface RequestCartItem {
-  collectionName: string;
-  itemName?: string;
-  readingRoomLocation?: string;
+export interface Canvas {
+  id: string;
+}
+
+interface WindowConfig {
+  allowClose: boolean;
+  allowFullscreen: boolean;
+  allowMaximize: boolean;
+  panels: {
+    info: boolean;
+    canvas: boolean;
+  };
+  canvasLink: CanvasLinkConfig;
+  defaultView: string;
+  views: Array<{
+    key: string;
+    behaviors?: string[];
+  }>;
+  allowTopCollectionButton?: boolean;
+}
+
+interface CanvasLinkConfig {
+  active: boolean;
+  enabled: boolean;
+  singleCanvasOnly: boolean;
+  providers: unknown[];
+  getCanvasLink: (manifestId: string, canvases: Canvas[]) => string;
+}
+
+interface MiradorWindow {
+  manifestId: string;
+  canvasId: string | null;
 }
