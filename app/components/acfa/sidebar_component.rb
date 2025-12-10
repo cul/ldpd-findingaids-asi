508c7c849422aeb60063e7973a6118cc4b5ecf46
Weronika Tomaszewska <wt2392@columbia.edu>
2025-12-10 17:13:49 -0500

    ACFA-679 rename sidebar search id

--- app/components/acfa/sidebar_component.rb
+++ app/components/acfa/sidebar_component.rb
@@ -16,7 +16,7 @@ module Acfa
       sb = Acfa::SearchBarComponent.new(
         url: helpers.search_action_url,
         params: { repository_id: repository_id, f: { collection: [collection_name] } },
-        form_options: { html: { id: 'search-within-a-collection' } }
+        form_options: { html: { id: 'collection-search-form' } }
       )
       sb.with_search_button do
         '<button class="btn btn-primary search-btn align-items-center" type="submit" id="search"><span class="d-none d-md-inline me-sm-1 submit-search-text visually-hidden">Search</span><i class="fa-regular fa-lg fa-folder-magnifying-glass"></i></button>'.html_safe
