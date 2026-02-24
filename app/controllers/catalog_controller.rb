2152524db7ea8130a16fc6466043dd544c604fc0
Bonnie Gordon <blg2144@columbia.edu>
2026-02-24 10:33:58 -0500

    change related field to component field

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -331,7 +331,7 @@ class CatalogController < ApplicationController
     config.add_component_field 'originalsloc', field: 'originalsloc_html_tesm', helper_method: :render_html_tags
     config.add_component_field 'relatedmaterial', field: 'relatedmaterial_html_tesm', helper_method: :render_html_tags
     config.add_component_field 'odd', field: 'odd_html_tesm', helper_method: :render_html_tags
-    config.add_related_field 'separatedmaterial', field: 'separatedmaterial_html_tesm', helper_method: :render_html_tags
+    config.add_component_field 'separatedmaterial', field: 'separatedmaterial_html_tesm', helper_method: :render_html_tags
 
 
     # Component Show Page - Indexed Terms Section
